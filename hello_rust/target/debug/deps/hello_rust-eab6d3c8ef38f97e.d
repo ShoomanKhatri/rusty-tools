1b4e90c0ff1d11220615e1f0bdbeb1cd53c145df
Shooman Khatri <112597601+ShoomanKhatri@users.noreply.github.com>
2026-01-31 19:51:31 +0000

    feat: setup  rust

---
+++ hello_rust/target/debug/deps/hello_rust-eab6d3c8ef38f97e.d
@@ -0,0 +1,5 @@
+/workspaces/rusty-tools/hello_rust/target/debug/deps/hello_rust-eab6d3c8ef38f97e.d: src/main.rs
+
+/workspaces/rusty-tools/hello_rust/target/debug/deps/hello_rust-eab6d3c8ef38f97e: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/workspaces/rusty-tools/hello_rust/target/debug/hello_rust: /workspaces/rusty-tools/hello_rust/src/main.rs
